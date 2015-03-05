704b9a4ef2be80c48d5e88d2edf1b1682129bc99
Gouga34 <imala34@hotmail.fr>
2015-03-05 12:10:59 +0100

    Evaluateur de si une main a gagné ou non face à une autre. Ne gère pas encore l'égalité entre deux mains.
    (references #31)

---
+++ Application/include/Evaluateur/Evaluateur.h
@@ -0,0 +1,28 @@
+/*========================================================================
+Nom: Evaluateur.h       Auteur: Morgane Vidal
+Maj: 04/03/2015          Creation: 04/03/2015  
+Projet: Profilage par essais et erreurs au poker
+--------------------------------------------------------------------------
+Specification: Classe correspondant l'évaluateur de main, compare 
+				deux mains et donne celle ayant le poids le plus fort
+=========================================================================*/
+
+#ifndef EVALUATEUR_H
+#define EVALUATEUR_H
+
+#include "CartesJoueur.h"
+#include "../Jeu/Carte.h"
+#include <iostream>
+#include <vector>
+
+class Evaluateur{
+	public:
+		/**
+		* @action compare les deux mains pour voir laquelle a le poids le plus fort
+		* @return true si main1 a gagné, false sinon
+		*/
+		static bool comparerMains(std::vector<Carte> table, std::vector<Carte> mainJoueur1, std::vector<Carte> mainJoueur2);
+
+};
+
+#endif
---
+++ Application/src/Evaluateur/Evaluateur.cpp
@@ -0,0 +1,39 @@
+/*========================================================================
+Nom: Evaluateur.h       Auteur: Morgane Vidal
+Maj: 04/03/2015          Creation: 04/03/2015  
+Projet: Profilage par essais et erreurs au poker
+--------------------------------------------------------------------------
+Specification: Fichier contenant les définitions de la classe Evaluateur.
+=========================================================================*/
+#include "../../include/Evaluateur/Evaluateur.h"
+
+using namespace std;
+
+bool Evaluateur::comparerMains(vector<Carte> table, vector<Carte> mainJoueur1, vector<Carte> mainJoueur2){
+
+	CartesJoueur main1(table, mainJoueur1);
+
+	CartesJoueur main2(table, mainJoueur2);
+
+	if(main1.getCombinaison()>main2.getCombinaison()){
+			return true;
+
+		
+		}
+		else if(main1.getCombinaison()<main2.getCombinaison()){
+			return false;
+		
+		}
+		else{
+			if(main1.getCombinaison()==1 && main2.getCombinaison()==1){
+				if(main1.getPoids()>main2.getPoids()){
+					return true;
+				}
+				else{
+					cout<<main1.getPoids()<<"<"<<main2.getPoids()<<endl;
+					return false;
+					
+				}
+			}
+		}
+}
