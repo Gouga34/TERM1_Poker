fbc63f0a66003576678b6dfff5179494d1e4933b
Manouel <manuel.cha34@gmail.com>
2015-05-22 09:20:35 +0200

    Classe abstraite ContenuFenetre

--- Application/include/Interface/ContenuFenetre.h
+++ Application/include/Interface/ContenuFenetre.h
@@ -1,4 +1,41 @@
+/*========================================================================
+Nom: ContenuFenetre.h       Auteur: Manuel CHATAIGNER
+Maj: 20/05/2015          Creation: 20/05/2015
+Projet: Profilage par essais et erreurs au poker
+--------------------------------------------------------------------------
+Specification: Classe définissant la fenêtre de choix des cartes.
+=========================================================================*/
+
 #ifndef CONTENUFENETRE_H
 #define CONTENUFENETRE_H
+
+#include "../Jeu/Jeu.h"
+#include <QWidget>
+#include <QTextEdit>
+
+class ContenuFenetre : public QWidget
+{
+    protected:
+
+        Jeu *jeu;
+
+        QTextEdit logs;
+
+    public:
+
+        ContenuFenetre(Jeu *j);
+        virtual ~ContenuFenetre();
+
+        /**
+         * @action Ajoute dans les logs le contenu passé en paramètre
+         * @param contenu Texte à ajouter aux logs
+         */
+        void ajoutLogs(QString contenu);
+
+        /**
+         * @brief Met à jour l'interface
+         */
+        virtual void actualiser() = 0;
+};
 
 #endif // CONTENUFENETRE_H
