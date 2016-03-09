018f8270a21c77e0f3d656e13b84d48609efce73
Gouga34 <morgane.vidal@wanadoo.fr>
2016-03-09 14:45:42 +0100

    tests unitaires quinte flush

--- Application/tests/TestsAssessor/tst_testsassessor.cpp
+++ Application/tests/TestsAssessor/tst_testsassessor.cpp
@@ -12,21 +12,17 @@ class TestsAssessor : public QObject
 public:
     TestsAssessor();
 
-private:
-    virtual std::vector<Carte> getTable();
-    virtual std::vector<Carte> getHand1();
-    virtual std::vector<Carte> getHand2();
-
 private Q_SLOTS:
     void compareHands();
-    void straight(); //Quinte Flush Royale
+    void royalFlush(); //Quinte Flush Royale
+    void straightFlush(); //Quinte Flush
 };
 
 TestsAssessor::TestsAssessor() {
 
 }
 
-std::vector<Carte> TestsAssessor::getTable() {
+void TestsAssessor::compareHands() {
     std::vector<Carte> table {
         {RANG_CARTE::Q, COULEUR_CARTE::COEUR},
         {RANG_CARTE::J, COULEUR_CARTE::COEUR},
@@ -34,41 +30,56 @@ std::vector<Carte> TestsAssessor::getTable() {
         {9, COULEUR_CARTE::PIQUE},
         {3, COULEUR_CARTE::TREFLE}
     };
-    return table;
-}
 
-std::vector<Carte> TestsAssessor::getHand1() {
     std::vector<Carte> hand1 {
         {RANG_CARTE::AS, COULEUR_CARTE::COEUR},
         {RANG_CARTE::K, COULEUR_CARTE::COEUR}
     };
-    return hand1;
-}
 
-std::vector<Carte> TestsAssessor::getHand2() {
     std::vector<Carte> hand2 {
         {10, COULEUR_CARTE::PIQUE},
         {10, COULEUR_CARTE::TREFLE}
     };
-    return hand2;
+
+    QCOMPARE(Evaluateur::comparerMains(table, hand1, hand2), RESULTAT_PARTIE::GAGNE);
+    QCOMPARE(Evaluateur::comparerMains(table, hand2, hand1), RESULTAT_PARTIE::PERDU);
+    QCOMPARE(Evaluateur::comparerMains(table, hand1, hand1), RESULTAT_PARTIE::EGALITE);
 }
 
-void TestsAssessor::compareHands() {
-    std::vector<Carte> table = getTable();
-    std::vector<Carte> hand1 = getHand1();
-    std::vector<Carte> hand2 = getHand2();
+void TestsAssessor::royalFlush() {
+    std::vector<Carte> table {
+        {RANG_CARTE::Q, COULEUR_CARTE::COEUR},
+        {RANG_CARTE::J, COULEUR_CARTE::COEUR},
+        {10, COULEUR_CARTE::COEUR},
+        {9, COULEUR_CARTE::PIQUE},
+        {3, COULEUR_CARTE::TREFLE}
+    };
 
-    QCOMPARE(RESULTAT_PARTIE::GAGNE, Evaluateur::comparerMains(table, hand1, hand2));
-    QCOMPARE(RESULTAT_PARTIE::PERDU, Evaluateur::comparerMains(table, hand2, hand1));
-    QCOMPARE(RESULTAT_PARTIE::EGALITE, Evaluateur::comparerMains(table, hand1, hand1));
-}
-
-void TestsAssessor::straight() {
-    std::vector<Carte> table = getTable();
-    std::vector<Carte> hand1 = getHand1();
+    std::vector<Carte> hand1 {
+        {RANG_CARTE::AS, COULEUR_CARTE::COEUR},
+        {RANG_CARTE::K, COULEUR_CARTE::COEUR}
+    };
 
     CartesJoueur cartes(table, hand1);
-    QCOMPARE(FORCE_MAIN::QUINTE_FLUSH_ROYALE, cartes.getCombinaison());
+    QCOMPARE(cartes.getCombinaison(), FORCE_MAIN::QUINTE_FLUSH_ROYALE);
+}
+
+void TestsAssessor::straightFlush() {
+    std::vector<Carte> table {
+        {RANG_CARTE::Q, COULEUR_CARTE::COEUR},
+        {RANG_CARTE::J, COULEUR_CARTE::COEUR},
+        {10, COULEUR_CARTE::COEUR},
+        {9, COULEUR_CARTE::PIQUE},
+        {3, COULEUR_CARTE::TREFLE}
+    };
+
+    std::vector<Carte> hand1 {
+        {9, COULEUR_CARTE::COEUR},
+        {RANG_CARTE::K, COULEUR_CARTE::COEUR}
+    };
+
+    CartesJoueur cartes(table, hand1);
+    QCOMPARE(cartes.getCombinaison(), FORCE_MAIN::QUINTE_FLUSH);
 }
 
 QTEST_APPLESS_MAIN(TestsAssessor)
